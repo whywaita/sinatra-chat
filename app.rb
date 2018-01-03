4b4b97a8cb4eeec3e059109000a14f7843b57961
whywaita <whywaita@whywrite.it>
2018-01-03 20:27:52 +0900

    add login function

--- app.rb
+++ app.rb
@@ -3,22 +3,71 @@
 require 'sinatra'
 require 'haml'
 require 'csv'
+require 'warden'
 
 Encoding.default_external = "UTF-8"
+enable :sessions
 
-get '/' do
+Warden::Strategies.add :login_test do
+  def valid?
+    params["name"] || params["password"]
+  end
+
+  def authenticate!
+    if params != ""
+      user = {
+        :name => params["name"],
+        :password => params["password"]
+      }
+      success!(user)
+    else
+      fail!("Could not login")
+    end
+  end
+end
+
+use Warden::Manager do |manager|
+  manager.default_strategies :login_test
+
+  manager.failure_app = Sinatra::Application
+end
+
+get "/" do
+  if request.env["warden"].user.nil?
+    haml :login
+  else
+    redirect "/bbs"
+  end
+end
+
+post "/login" do
+  request.env["warden"].authenticate!
+  redirect "/bbs"
+end
+
+get "/logout" do
+  request.env["warden"].logout
+  redirect "/"
+end
+
+get '/bbs' do
+  if request.env["warden"].user.nil?
+    redirect "/"
+  end
+
   @title = "sinatra chat"
   @text = "Hello haml"
   @bbs_data = CSV.read("resources/bbs_data.csv")
+
   haml :index
 end
 
-post '/' do
+post '/bbs' do
   CSV.open("resources/bbs_data.csv", "a") do |f|
-    f << [params[:name], params[:title], params[:message]]
+    f << [request.env["warden"].user[:name], params[:title], params[:message]]
   end
 
-  redirect to('/')
+  redirect "/bbs"
 end
 
 get '/user/:name' do |name|
