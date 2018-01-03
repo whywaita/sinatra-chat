f91e885381b3c99e21d5dd4a2e35b9a7c8b82d73
whywaita <whywaita@whywrite.it>
2018-01-03 19:32:04 +0900

    write bbs_data when post chat

--- app.rb
+++ app.rb
@@ -1,32 +1,32 @@
 # encoding: utf-8
 
 require 'sinatra'
-require 'sinatra/reloader' if development?
+require 'sinatra/reloader'
 require 'haml'
 require 'csv'
 
 Encoding.default_external = "UTF-8"
 
 get '/' do
-  @title = "Hello world!"
+  @title = "sinatra chat"
   @text = "Hello haml"
   @bbs_data = CSV.read("resources/bbs_data.csv")
   haml :index
 end
 
-put '/request_print' do
-  @request_method = request.request_method
-  @request_url = request.url
-  @request_path = request.path
+put '/' do
+  @title = "sinatra chat"
+  @text = "Hello haml"
+  @bbs_data = CSV.read("resources/bbs_data.csv")
 
-  @name = params[:name]
-  @title = params[:title]
-  @messeage = params[:messeage]
+  CSV.open("resources/bbs_data.csv", "a") do |f|
+    f << [params[:name], params[:title], params[:message]]
+  end
 
-  redirect to('/login/' + params[:name])
+  redirect to('/')
 end
 
-get '/login/:name' do |name|
+get '/user/:name' do |name|
   "hello, #{name}!"
 end
 
